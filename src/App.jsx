9a2e1247d04a98eb075c39845d26eedc4a99eca7
tvvmvn <tvvmvn@gmail.com>
2025-05-13 20:17:57 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+export default function App() {
+
+  return (
+    <>
+      <h1>Firebase App</h1>
+    </>
+  )
+}
+
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBexVX73dB4nMnf-VisFIAmLtkxH12W0dU",
+  authDomain: "instagram-8a332.firebaseapp.com",
+  projectId: "instagram-8a332",
+  storageBucket: "instagram-8a332.firebasestorage.app",
+  messagingSenderId: "588187827486",
+  appId: "1:588187827486:web:16baa90de21f03f1a79d0f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
