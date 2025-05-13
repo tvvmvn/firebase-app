3e645f0bffc338cdb07fc7ac6cd85b4eb43229ce
tvvmvn <tvvmvn@gmail.com>
2025-05-13 20:55:00 +0900

    5.13

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import { useState } from 'react'
+import { auth } from './firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth'
 
 export default function App() {
+
+  const email = "johndoe@example.com"
+  const [password, setPassword] = useState("")
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    try {
+      const userCrendential = await signInWithEmailAndPassword(auth, email, password)
+      const user = userCrendential.user;
+
+      alert(user.uid)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   return (
     <>
       <h1>Firebase App</h1>
+      <form onSubmit={handleSubmit}>
+        <h3>Login</h3>
+        <p>{email}</p>
+        <input 
+          type="password" 
+          name="" 
+          id="" 
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button disabled={!password}>
+          Login
+        </button>
+      </form>
     </>
   )
 }
